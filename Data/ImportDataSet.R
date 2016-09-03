7704cd24883a32094e62c5aed627910126fbbcfc
TonyRoberson <tonyjroberson@gmail.com>
2016-09-03 18:03:07 -0500

    Cleaned formatting

--- Data/ImportDataSet.R
+++ Data/ImportDataSet.R
@@ -1,3 +1,7 @@
 # Import cleaned and merged thesis data set
+# Need to set appropriate working directory
+# where dataset lives
 
-thesis <- read.csv(file = "thesis_merged.csv", header = TRUE, stringsAsFactors = FALSE)
+thesis <- read.csv(file = "thesis_merged.csv", 
+                   header = TRUE, 
+                   stringsAsFactors = FALSE)
