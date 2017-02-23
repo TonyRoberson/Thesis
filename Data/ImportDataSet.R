0b3c5383665ba562205c5a9424b50fc0cd9008a6
TonyRoberson <tonyjroberson@gmail.com>
2017-02-23 14:12:50 -0600

    Delete redundant data import script

--- Data/ImportDataSet.R
+++
@@ -1,7 +0,0 @@
-# Import cleaned and merged thesis data set
-# Need to set appropriate working directory
-# where dataset lives
-
-thesis <- read.csv(file = "thesis_merged.csv", 
-                   header = TRUE, 
-                   stringsAsFactors = FALSE)
