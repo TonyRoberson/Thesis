6fe37586dcf919711db61ab3e106f88b500cf4bf
TonyRoberson <tonyjroberson@gmail.com>
2017-06-26 19:33:28 -0500

    1. Updated from deprecated fa.poly to fa 2. Added models with two Heywood cases removed

--- AnalysisScripts/thesis_EFA.R
+++ AnalysisScripts/thesis_EFA.R
@@ -5,7 +5,6 @@ library(psych)
 library(dplyr)
 library(random.polychor.pa)
 library(lavaan)
-library(MBESS)
 
 ## Import cleaned and merged data set
 thesis <- read.csv(file = "thesis.csv", 
@@ -52,9 +51,9 @@ swtrs.efa <- data.frame(
 
 ## Check results from unconstrained principal axis factoring analysis
 ## using the polychoric correlation matrix
-efa.pa <- fa.poly(x = swtrs.efa, 
-                  fa = "pa", 
-                  rotate = "promax")
+efa.pa <- fa(r = swtrs.efa,
+             cor = "poly", 
+             fm = "pa")
 # Kaiser-Meyer-Olkin Sampling Adequacy
 KMO(swtrs.efa)
 # Sphericity
@@ -70,16 +69,21 @@ random.polychor.pa(nvar = 31,
                    q.eigen = .95)
 # Inspect polychoric correlation matrix
 efa.pa$rho
+# Print Unidimensional solution
+print.psych(x = efa.pa,
+            digits = 3, 
+            sort = TRUE)
 
 
 ## Check 3 factor solution
-efa.out.3f <- fa.poly(x = swtrs.efa, 
+efa.out.3f <- fa(r = swtrs.efa, 
                       fm = "pa", 
                       nfactors = 3, 
                       rotate = "promax", 
-                      residual = TRUE)
+                      residuals = TRUE, 
+                      cor = "poly")
 # Inspect model residuals
-resid.3f <- as.matrix(efa.out.3f$fa$residual)
+resid.3f <- as.matrix(efa.out.3f$residual)
 describe(resid.3f)
 boxplot(resid.3f)
 # Print summary
@@ -89,13 +93,14 @@ print.psych(x = efa.out.3f,
 
 
 ## Check 2 factor solution
-efa.out.2f <- fa.poly(x = swtrs.efa, 
-                      fm = "pa", 
-                      nfactors = 2, 
-                      rotate = "promax", 
-                      residual = TRUE)
+efa.out.2f <- fa(r = swtrs.efa, 
+                  fm = "pa", 
+                  nfactors = 2, 
+                  rotate = "promax", 
+                  residual = TRUE, 
+                  cor = "poly")
 # Inspect model residuals
-resid.2f <- as.matrix(efa.out.2f$fa$residual)
+resid.2f <- as.matrix(efa.out.2f$residual)
 describe(resid.2f)
 boxplot(resid.2f)
 # Print summary
@@ -105,20 +110,20 @@ print.psych(x = efa.out.2f,
 
 
 ## Check 4 factor solution
-efa.out.4f <- fa.poly(x = swtrs.efa, 
-                      fm = "pa", 
-                      nfactors = 4, 
-                      rotate = "promax", 
-                      residual = TRUE)
+efa.out.4f <- fa(r = swtrs.efa, 
+                    fm = "pa", 
+                    nfactors = 4, 
+                    rotate = "promax", 
+                    residual = TRUE,
+                    cor = "poly")
 # Inspect model residuals
-resid.4f <- as.matrix(efa.out.4f$fa$residual)
+resid.4f <- as.matrix(efa.out.4f$residual)
 describe(resid.4f)
 boxplot(resid.4f)
 # Print summary
 print.psych(x = efa.out.4f, 
             digits = 3, 
             sort = TRUE)
-
 
 #### Reduced EFA 1 ####
 
@@ -150,25 +155,26 @@ swtrs.reduced.1 <- data.frame(
             "swtrs_enjoysGroupWorkWithOthers",
             "swtrs_sociableWithOthersDuringFreeTime",
             "swtrs_needsLittleSupervision",
-            "swtrs_comfortableWorkingIndependently",
             "swtrs_inquisitiveInterestedLearningNewThings",
             "swtrs_laughsAppropriateTimes",
             "swtrs_smilesAtSchool",
-            "swtrs_seemsHappyInClass"
+            "swtrs_seemsHappyInClass",
+            "swtrs_comfortableWorkingIndependently"
   )])
 
 
 ## Check results from unconstrained principal axis factoring analysis
 ## using the polychoric correlation matrix
-efa.pa.r1 <- fa.poly(x = swtrs.reduced.1, 
-                  fm = "pa", 
-                  rotate = "promax")
+efa.pa.r1 <- fa(r = swtrs.reduced.1, 
+                fm = "pa", 
+                rotate = "promax", 
+                cor = "poly")
 # Kaiser-Meyer-Olkin Sampling Adequacy
 KMO(swtrs.reduced.1)
 # Sphericity
 cortest.bartlett(swtrs.reduced.1)
 # Eigenvalues
-efa.pa.r1$fa$values
+efa.pa.r1$values
 # Scree plot and parallel analysis
 # Suggested factors = 3
 random.polychor.pa(nvar = 25, 
@@ -179,19 +185,20 @@ random.polychor.pa(nvar = 25,
 
 
 ## Check 3 factor solution
-efa.out.3f.b <- fa.poly(x = swtrs.reduced.1,
-                        fm = "pa",
-                        nfactors = 3,
-                        rotate = "promax",
-                        residual = TRUE)
+efa.out.3f.b <- fa(r = swtrs.reduced.1,
+                   fm = "pa",
+                   nfactors = 3,
+                   rotate = "promax",
+                   residual = TRUE, 
+                   cor = "poly")
 # Inspect model residuals
-resid.3f.b <- as.matrix(efa.out.3f.b$fa$residual)
+resid.3f.b <- as.matrix(efa.out.3f.b$residual)
 describe(resid.3f.b)
 boxplot(resid.3f.b)
 # Print summary
 print.psych(x = efa.out.3f.b,
-                digits = 3,
-                sort = TRUE)
+            digits = 3,
+            sort = TRUE)
 
 
 #### Reduced EFA 2: Preferred Structure! ####
@@ -220,49 +227,142 @@ swtrs.reduced.2 <- data.frame(
             "swtrs_enjoysGroupWorkWithOthers",
             "swtrs_sociableWithOthersDuringFreeTime",
             "swtrs_needsLittleSupervision",
-            "swtrs_comfortableWorkingIndependently",
             "swtrs_smilesAtSchool",
             "swtrs_seemsHappyInClass",
-            "swtrs_inquisitiveInterestedLearningNewThings"
+            "swtrs_inquisitiveInterestedLearningNewThings",
+            "swtrs_comfortableWorkingIndependently"
   )])
 
 ## Check results from unconstrained principal axis factoring analysis
 ## using the polychoric correlation matrix
-efa.pa.r2 <- fa.poly(x = swtrs.reduced.2, 
-                  fm = "pa", 
-                  rotate = "promax")
+efa.pa.r2 <- fa(r = swtrs.reduced.2, 
+                fm = "pa", 
+                rotate = "promax",
+                cor = "poly",
+                residuals = TRUE)
 # Kaiser-Meyer-Olkin Sampling Adequacy
 KMO(swtrs.reduced.2)
 # Sphericity
 cortest.bartlett(swtrs.reduced.2)
 # Eigenvalues
-efa.pa.r2$fa$values
+efa.pa.r2$values
 # Scree plot and parallel analysis
 # Suggested factors = 3
 random.polychor.pa(nvar = 17, 
                    n.ss = 184, 
-                   nrep = 10,
+                   nrep = 1000,
                    data.matrix = swtrs.reduced.2, 
-                   q.eigen = .95)
+                   q.eigen = 0.95)
+print(efa.pa.r2, digits = 3, sort = TRUE)
 
 ## Check 3 factor solution
-efa.out.3f.d <- fa.poly(x = swtrs.reduced.2,
-                        fm = "pa",
-                        nfactors = 3,
-                        rotate = "promax",
-                        residual = TRUE)
+efa.out.3f.d <- fa(r = swtrs.reduced.2,
+                   fm = "pa",
+                   nfactors = 3,
+                   rotate = "promax",
+                   residual = TRUE,
+                   cor = "poly")
 # Inspect model residuals
-resid.3f.d <- as.matrix(efa.out.3f.d$fa$residual)
+resid.3f.d <- as.matrix(efa.out.3f.d$residual)
 describe(resid.3f.d)
 boxplot(resid.3f.d)
 # Print summary
 print.psych(x = efa.out.3f.d,
+            digits = 3,
+            sort = TRUE)
+
+
+#### Reduced EFA 1b: Heywood removed ####
+
+## Remove high cross-loading items:
+# Follows Directions
+# Alert during lessons
+# Stays focused
+# Willing to try new activities
+# Optimistic will succeed in school
+## Remove Heywood case
+# Comfortable working independently
+
+swtrs.reduced.1b <- data.frame(
+  thesis[,c("swtrs_listensToTeachers",
+            "swtrs_handsFeetToSelf",
+            "swtrs_treatsClassmatesKindly",
+            "swtrs_playsWellWithOthers",
+            "swtrs_handlesFrustrationsWell",
+            "swtrs_peacefulDuringClass",
+            "swtrs_respectfulToTeachers",
+            "swtrs_participatesMeaningfully",
+            "swtrs_wellBehavedDuringClass",
+            "swtrs_staysOnTask",
+            "swtrs_engagedInLearning",
+            "swtrs_confidentNewChallengingMaterial",
+            "swtrs_remainsCalmDifficultSituation",
+            "swtrs_seemsRelaxedAtEase",
+            "swtrs_classmatesLikeWorkPlayWithThem",
+            "swtrs_classmatesRespectfulToThem",
+            "swtrs_approachableEasyGetAlongWith",
+            "swtrs_enjoysGroupWorkWithOthers",
+            "swtrs_sociableWithOthersDuringFreeTime",
+            "swtrs_needsLittleSupervision",
+            "swtrs_inquisitiveInterestedLearningNewThings",
+            "swtrs_laughsAppropriateTimes",
+            "swtrs_smilesAtSchool",
+            "swtrs_seemsHappyInClass"
+  )])
+
+
+## Check results from unconstrained principal axis factoring analysis
+## using the polychoric correlation matrix
+efa.pa.r1b <- fa(r = swtrs.reduced.1b, 
+                  fm = "pa", 
+                  rotate = "promax", 
+                cor = "poly")
+# Kaiser-Meyer-Olkin Sampling Adequacy
+KMO(swtrs.reduced.1b)
+# Sphericity
+cortest.bartlett(swtrs.reduced.1b)
+# Eigenvalues
+efa.pa.r1b$values
+# Scree plot and parallel analysis
+# Suggested factors = 3
+random.polychor.pa(nvar = 24, 
+                   n.ss = 184, 
+                   nrep = 10, 
+                   data.matrix = swtrs.reduced.1b, 
+                   q.eigen = .95)
+
+
+## Check 3 factor solution
+efa.out.3f.b2 <- fa(r = swtrs.reduced.1b,
+                        fm = "pa",
+                        nfactors = 3,
+                        rotate = "promax",
+                        residual = TRUE, 
+                        cor = "poly")
+# Inspect model residuals
+resid.3f.b2 <- as.matrix(efa.out.3f.b2$residual)
+describe(resid.3f.b2)
+boxplot(resid.3f.b2)
+# Print summary
+print.psych(x = efa.out.3f.b2,
                 digits = 3,
                 sort = TRUE)
 
-#### EFA for SIBS/SEBS + SWTRS ####
 
-pth.wb <- data.frame(
+#### Reduced EFA 2b: Heywood removed ####
+
+## Items removed:
+# Respectful to teachers
+# Laughs at appropriate times
+# Peaceful during class
+# Remains calm in dif. situations
+# Classmates respectful to them
+# Plays well with others
+# Stays on task
+## Heywood case:
+# Hands feet to self
+
+swtrs.reduced.2b <- data.frame(
   thesis[,c("swtrs_listensToTeachers",
             "swtrs_treatsClassmatesKindly",
             "swtrs_handlesFrustrationsWell",
@@ -276,148 +376,48 @@ pth.wb <- data.frame(
             "swtrs_enjoysGroupWorkWithOthers",
             "swtrs_sociableWithOthersDuringFreeTime",
             "swtrs_needsLittleSupervision",
-            "swtrs_comfortableWorkingIndependently",
             "swtrs_smilesAtSchool",
             "swtrs_seemsHappyInClass",
-            "swtrs_inquisitiveInterestedLearningNewThings",
-            "sibs_nervousWorriedFearful",
-            "sibs_bullied",
-            "sibs_spendsTimeAlone",
-            "sibs_clingsToAdults",
-            "sibs_withdrawn",
-            "sibs_sadUnhappy",
-            "sibs_complainsSickHurt",
-            "sebs_bulliesOthers",
-            "sebs_defiantOppositional",
-            "sebs_fightsArgues",
-            "sebs_easilyAngry",
-            "sebs_lies",
-            "sebs_disruptsClass",
-            "sebs_sittingStillDifficult"
+            "swtrs_inquisitiveInterestedLearningNewThings"
   )])
 
 ## Check results from unconstrained principal axis factoring analysis
 ## using the polychoric correlation matrix
-efa.pth.wb <- fa.poly(x = pth.wb, 
+efa.pa.r2b <- fa(r = swtrs.reduced.2b, 
                      fm = "pa", 
-                     rotate = "promax")
+                     rotate = "promax",
+                     cor = "poly",
+                    residuals = TRUE)
 # Kaiser-Meyer-Olkin Sampling Adequacy
-KMO(pth.wb)
+KMO(swtrs.reduced.2b)
 # Sphericity
-cortest.bartlett(pth.wb)
+cortest.bartlett(swtrs.reduced.2b)
 # Eigenvalues
-efa.pth.wb$fa$values
+efa.pa.r2b$values
 # Scree plot and parallel analysis
 # Suggested factors = 3
-random.polychor.pa(nvar = 31, 
+random.polychor.pa(nvar = 16, 
                    n.ss = 184, 
-                   nrep = 10, 
-                   data.matrix = pth.wb, 
-                   q.eigen = .95)
-  
+                   nrep = 10,
+                   data.matrix = swtrs.reduced.2b, 
+                   q.eigen = 0.95)
+print(efa.pa.r2b, 
+      digits = 3, 
+      sort = TRUE)
 
 ## Check 3 factor solution
-efa.pthwb.out.3f <- fa.poly(x = pth.wb,
+efa.out.3f.d2 <- fa(r = swtrs.reduced.2b,
                         fm = "pa",
                         nfactors = 3,
                         rotate = "promax",
-                        residual = TRUE)
+                        residual = TRUE,
+                        cor = "poly")
 # Inspect model residuals
-resid.pthwb.3f <- as.matrix(efa.pthwb.out.3f$fa$residual)
-describe(resid.pthwb.3f)
-boxplot(resid.pthwb.3f)
+resid.3f.d2 <- as.matrix(efa.out.3f.d2$residual)
+describe(resid.3f.d2)
+boxplot(resid.3f.d2)
 # Print summary
-print.psych(x = efa.pthwb.out.3f,
-            digits = 3,
-            sort = TRUE)
-
-
-## Check 4 factor solution
-efa.pthwb.out.4f <- fa.poly(x = pth.wb,
-                            fm = "pa",
-                            nfactors = 4,
-                            rotate = "promax",
-                            residual = TRUE)
-# Inspect model residuals
-resid.pthwb.4f <- as.matrix(efa.pthwb.out.4f$fa$residual)
-describe(resid.pthwb.4f)
-boxplot(resid.pthwb.4f)
-# Print summary
-print.psych(x = efa.pthwb.out.4f,
-            digits = 3,
-            sort = TRUE)
-
-#### Reduced EFA for SIBS/SEBS + SWTRS ####
-
-## Items Removed
-# SEBS:  Bullies Others
-# SIBS:  Withdrawn
-#        Clings to adults
-#        Nervous worried fearful
-#        Bullied
-# SWTRS: Enjoys group work with others,
-#        Smiles at school
-#        Sociable with others during free time
-#        Seems Happy in class
-
-      
-pth.wb.2 <- data.frame(
-  thesis[,c("swtrs_listensToTeachers",
-            "swtrs_treatsClassmatesKindly",
-            "swtrs_handlesFrustrationsWell",
-            "swtrs_participatesMeaningfully",
-            "swtrs_wellBehavedDuringClass",
-            "swtrs_engagedInLearning",
-            "swtrs_confidentNewChallengingMaterial",
-            "swtrs_seemsRelaxedAtEase",
-            "swtrs_classmatesLikeWorkPlayWithThem",
-            "swtrs_approachableEasyGetAlongWith",
-            "swtrs_needsLittleSupervision",
-            "swtrs_comfortableWorkingIndependently",
-            "swtrs_inquisitiveInterestedLearningNewThings",
-            "sibs_spendsTimeAlone",
-            "sibs_sadUnhappy",
-            "sibs_complainsSickHurt",
-            "sebs_defiantOppositional",
-            "sebs_fightsArgues",
-            "sebs_easilyAngry",
-            "sebs_lies",
-            "sebs_disruptsClass",
-            "sebs_sittingStillDifficult"
-  )])
-
-## Check results from unconstrained principal axis factoring analysis
-## using the polychoric correlation matrix
-efa.pth.wb.2 <- fa.poly(x = pth.wb.2, 
-                      fm = "pa", 
-                      rotate = "promax")
-# Kaiser-Meyer-Olkin Sampling Adequacy
-KMO(pth.wb.2)
-# Sphericity
-cortest.bartlett(pth.wb.2)
-# Eigenvalues
-efa.pth.wb.2$fa$values
-# Scree plot and parallel analysis
-# Suggested factors = 3
-random.polychor.pa(nvar = 22, 
-                   n.ss = 184, 
-                   nrep = 10, 
-                   data.matrix = pth.wb.2, 
-                   q.eigen = .95)
-
-
-## Check 3 factor solution
-efa.pthwb.out.3f.2 <- fa.poly(x = pth.wb.2,
-                            fm = "pa",
-                            nfactors = 3,
-                            rotate = "promax",
-                            residual = TRUE)
-# Inspect model residuals
-resid.pthwb.3f.2 <- as.matrix(efa.pthwb.out.3f.2$fa$residual)
-describe(resid.pthwb.3f.2)
-boxplot(resid.pthwb.3f.2)
-# Print summary
-print.psych(x = efa.pthwb.out.3f.2,
-            digits = 3,
-            sort = TRUE)
+print.psych(x = efa.out.3f.d2,
+                digits = 3,
+                sort = TRUE)
 
